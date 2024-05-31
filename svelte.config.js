824f37c2b2471245eccf1cb6a05a8d535d980fc6
isaackd <afrmtbl@gmail.com>
2024-05-31 18:53:25 -0400

    Fix paths... again..

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,7 @@ const config = {
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
         adapter: adapter({
             pages: "docs",
-            assets: "docs/arcus",
+            assets: "docs",
             fallback: null,
             precompress: false,
             strict: true,
