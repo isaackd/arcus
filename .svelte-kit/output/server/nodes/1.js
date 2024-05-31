2e9fa6fde0fdbb1ae0e3e533295a2ee874733242
isaackd <afrmtbl@gmail.com>
2024-05-31 18:48:38 -0400

    Remove .svelte-kit and .vscode

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["app/immutable/nodes/1.ba1d9522.js","app/immutable/chunks/scheduler.92d9c31b.js","app/immutable/chunks/index.0055131f.js","app/immutable/chunks/entry.38169856.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["app/immutable/nodes/2.6182cac4.js","app/immutable/chunks/scheduler.92d9c31b.js","app/immutable/chunks/index.0055131f.js"];
-export const stylesheets = ["app/immutable/assets/2.6f98685e.css"];
-export const fonts = [];
